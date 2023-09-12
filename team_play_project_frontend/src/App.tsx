b12186bff500ecbad4836612f217d7c6f31358bf
Manzo48 <magomed.shocoladka@mail.ru>
2023-09-12 16:41:47 +0300

    Теперь рендер постов закончен, исправлены проблемы с типизацией от моей стороны, боюсь увидеть че у вас там

--- team_play_project_frontend/src/App.tsx
+++ team_play_project_frontend/src/App.tsx
@@ -1,19 +1,19 @@
 import { Route, Routes } from "react-router";
 import Header from "./components/Comments/Header";
-import HomePage from "./components/Comments/HomePage";
-import style from './css/app.module.css'
+import HomePage from "./pages/HomePage/HomePage";
+import style from "./css/app.module.css";
+import FullPost from "./pages/HomePage/FullPostPage";
 
 function App() {
-
   return (
     <div className={style.app}>
-    <Header/>
-    <Routes>
-      <Route path='/' element={<HomePage/>}>
-      </Route>
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/fullpost/:postId" element={<FullPost />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
